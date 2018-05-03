a17ed9b593aff0d829cb2ddb0caeff7fee3776ed
Shubham Wahal <wahalshubham@gmail.com>
2018-05-03 23:39:47 +0530

    Angular Components part-2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpModule} from '@angular/http';
+
+import { MaterialModule } from '@angular/material'; 
+import {FlexLayoutModule} from '@angular/flex-layout';
+
+import 'hammerjs';
+import * as Rx from 'rxjs';
 
 
 import { AppComponent } from './app.component';
+import { subscribeOn } from 'rxjs/operator/subscribeOn';
+import { MenuComponent } from './menu/menu.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    HttpModule,
+    FlexLayoutModule,
+      ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [
+    AppComponent,
+    MenuComponent
+
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
